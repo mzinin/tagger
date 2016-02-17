da103b6e749eb939df4405538982993fc08e0475
Mikhail Zinin <mzinin@gmail.com>
2016-02-17 22:45:27 +0300

    Windows version info and icons.

--- install.bat
+++ install.bat
@@ -1,12 +1,15 @@
 @ECHO off
 
-FOR /F "TOKENS=2" %%i IN ('FINDSTR /R /C:"\"Major\": *[0-9]*" version.json') DO SET @major=%%i
+FOR /F "TOKENS=2" %%i IN ('FINDSTR /R /C:"\"Major\": *[0-9]*" versioninfo.json') DO SET @major=%%i
 SET @major=%@major:~0,-1%
 
-FOR /F "TOKENS=2" %%i IN ('FINDSTR /R /C:"\"Minor\": *[0-9]*" version.json') DO SET @minor=%%i
+FOR /F "TOKENS=2" %%i IN ('FINDSTR /R /C:"\"Minor\": *[0-9]*" versioninfo.json') DO SET @minor=%%i
 SET @minor=%@minor:~0,-1%
 
-FOR /F "TOKENS=2" %%i IN ('FINDSTR /R /C:"\"Patch\": *[0-9]*" version.json') DO SET @patch=%%i
+FOR /F "TOKENS=2" %%i IN ('FINDSTR /R /C:"\"Patch\": *[0-9]*" versioninfo.json') DO SET @patch=%%i
 SET @patch=%@patch:~0,-1%
+
+go get github.com/josephspurrier/goversioninfo/cmd/goversioninfo
+%GOPATH%/bin/goversioninfo.exe -icon=icons/tagger.ico
 
 go install -ldflags "-X main.version=%@major%.%@minor%.%@patch%"
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-major=`cat ./version.json | grep -m 1 Major | grep -o '[0-9]*'`
-minor=`cat ./version.json | grep -m 1 Minor | grep -o '[0-9]*'`
-patch=`cat ./version.json | grep -m 1 Patch | grep -o '[0-9]*'`
+major=`cat ./versioninfo.json | grep -m 1 Major | grep -o '[0-9]*'`
+minor=`cat ./versioninfo.json | grep -m 1 Minor | grep -o '[0-9]*'`
+patch=`cat ./versioninfo.json | grep -m 1 Patch | grep -o '[0-9]*'`
 
 go install -ldflags "-X main.version=$major.$minor.$patch"
